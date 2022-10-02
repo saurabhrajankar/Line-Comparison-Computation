e8669f1b1c1af94d040750e8924b988696b4eb80
saurabhrajankar <saurabhrajankar777@gmail.com>
2022-10-02 23:07:14 +0530

    Welcome to Line Comparison Computation

---
+++ Welcome to Line Comparison Computation/Welcome to Line Comparison Computation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome_to_Line_Comparison_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.ReadLine();
+        }
+    }
+}
